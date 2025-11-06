df6afa94f0a79aabc524122c8c28d3d980414989
Oscar R. Acuña [Rhaenesis] <acunainfosec@proton.me>
2025-11-05 20:57:36 -0700

    Update prep_shell.sh

--- prep_shell.sh
+++ prep_shell.sh
@@ -3,16 +3,18 @@
 #TODO: function to determine dnf vs apt
 
 echo 'downloading git...'
-dnf install git -y
+sudo dnf install git -y
 
 echo 'downloading curl'
-dnf install curl -y
+sudo dnf install curl -y
 
 echo 'downloading fuse'
-dnf install fuse -y
+sudo dnf install fuse -y
 
 echo 'downloading zsh'
-dnf install zsh -y
+sudo dnf install zsh -y
+
+echo 'downloading wget'
 
 echo 'downloading oh-my-zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -21,8 +23,8 @@ echo 'downloading nvim app image'
 wget 'https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.appimage'
 
 echo 'renaming nvim app image, making it executable and moving to /usr/bin/'
-mv nvim-linux-x86_64.appimage /usr/bin/nvim.appimage 
-chmod +x /usr/bin/nvim.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/bin/nvim.appimage 
+sudo chmod +x /usr/bin/nvim.appimage
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
